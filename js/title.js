31c2b6f274c50ddc112e3ed47491ab1c6ea0ed9c
aligu99 <zhangsan200022@gmail.com>
2024-06-07 19:47:11 +0800

    Site updated: 2024-06-07 19:47:10

---
+++ js/title.js
@@ -0,0 +1,1 @@
+"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="⚽国足又倒了~",clearTimeout(titleTime)):(document.title="🐼又失眠了～",titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
