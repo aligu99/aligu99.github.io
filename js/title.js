2ae0f45eb3f5177eada9899cbb0ca7f33d03fe39
aligu99 <zhangsan200022@gmail.com>
2024-06-10 11:01:31 +0800

    Site updated: 2024-06-10 11:01:30

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-const OriginTitile = document.title;
-let titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '⚽国足又倒了~';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '🐼又失眠了～';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="⚽国足又倒了~",clearTimeout(titleTime)):(document.title="🐼又失眠了～",titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
